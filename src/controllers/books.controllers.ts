04b13b54e0164f5917a0434b5c70d6412e00475f
Daniel <dursuaf@gmail.com>
2023-02-24 18:58:54 +0100

    Add repo and controller

---
+++ src/controllers/books.controllers.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from 'express';
+import { BooksFileRepo } from '../repository/books.file.repo';
+
+export class BooksController {
+  // eslint-disable-next-line no-useless-constructor, no-unused-vars
+  constructor(public repo: BooksFileRepo) {}
+
+  getAll(_req: Request, resp: Response) {
+    this.repo.read().then((data) => {
+      resp.json(data);
+    });
+  }
+
+  get(req: Request, resp: Response) {
+    resp.send('This is a book ' + req.params.id);
+  }
+
+  post(_req: Request, _resp: Response) {}
+
+  patch(_req: Request, _resp: Response) {}
+
+  delete(_req: Request, _resp: Response) {}
+}
